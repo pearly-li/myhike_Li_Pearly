f59251f2c3e10fa7f1f9bb621d05a6e49ba987dd
Pearly Li <pearlyli@protonmail.com>
2024-10-15 12:15:16 -0700

    Add logout function

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,23 @@
 // Your own functions here
 //---------------------------------
 
-
 function sayHello() {
-    //do something
+  //do something
 }
 //sayHello();    //invoke function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+}
